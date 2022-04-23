b705d737c2409e20fb9b0fa47fc1266f3287e3b7
William Hobson <will.hobson85@gmail.com>
2022-04-23 12:11:16 -0400

    Update player classes

---
+++ player.js
@@ -0,0 +1,29 @@
+class ActivePlayer {
+  constructor(name, token, wins, active) {
+    this.name = name;
+    this.token =
+    this.wins = 0;
+    this.active = true;
+  }
+
+  increaseWins() {
+    if () {
+      this.wins === this.wins + 1;
+    }
+  }
+}
+
+class QueuedPlayer {
+  constructor(name, token, wins, active) {
+    this.name = name;
+    this.token =
+    this.wins = 0;
+    this.active = false;
+  }
+
+  increaseWins() {
+    if () {
+      this.wins === this.wins + 1;
+    }
+  }
+}
