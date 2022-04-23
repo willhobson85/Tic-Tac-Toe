e7eecec4889aca0a3e9404f8c3a46ad94fbd11ca
William Hobson <will.hobson85@gmail.com>
2022-04-23 12:47:30 -0400

    Remove second class

--- player.js
+++ player.js
@@ -1,24 +1,9 @@
-class ActivePlayer {
+class Player {
   constructor(name, token, wins, active) {
     this.name = name;
     this.token =
     this.wins = 0;
     this.active = true;
-  }
-
-  increaseWins() {
-    if () {
-      this.wins === this.wins + 1;
-    }
-  }
-}
-
-class QueuedPlayer {
-  constructor(name, token, wins, active) {
-    this.name = name;
-    this.token =
-    this.wins = 0;
-    this.active = false;
   }
 
   increaseWins() {
