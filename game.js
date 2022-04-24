c63001b2d55cdf795bbe73ea6cfdbc2781bdb386
William Hobson <will.hobson85@gmail.com>
2022-04-24 19:01:10 -0400

    Create functions to run game

--- game.js
+++ game.js
@@ -1,46 +1,66 @@
-class Game() {
-  constructor() {
+class Game {
+  constructor(player1, player2) {
     this.player1 = new Player(id, token);
     this.player2 = new Player(id, token);
   }
 
   makeMove(){
-    //innerText = currentPlayer
-
-    //place token
-    // disable box click
+    var currentPlayer = this.player1;
+    var gameGrid = document.querySelector(".grid");
+    var tiles = document.querySelectorAll(".grid div");
+    gameGrid.addEventListener('click', makeMove);
+    if (tiles[i].classList.contains('selected')) {
+      if (currentPlayer === player1) {
+        tiles[i].classList.add('selected');
+        tiles[i].classList.add(this.player1.token);
+        currentPlayer = player2;
+        tiles.innerHTML = diego;
+      } else if (currentPlayer === player2 ) {
+        tiles[i].classList.add('selected');
+        tiles[i].classList.add(this.player2.token);
+        currentPlayer = player1;
+        tiles.innerHTML = waldo;
+      } else alert('This tile is taken!');
+      checkWinDraw();
+    }
   }
 
   checkWinDraw() {
-    var winConditions = [
-      {"box1", "box2", "box3"},
-      {"box4", "box5", "box6"},
-      {"box7", "box8", "box9"},
-      {"box1", "box4", "box7"},
-      {"box2", "box5", "box8"},
-      {"box3", "box6", "box9"},
-      {"box1", "box5", "box9"},
-      {"box3", "box5", "box7"}
-    ];
-    if (something) {
-      winning();
-    } else if (something) {
-      tiedGame();
-    } else {
-      makeMove();
+    for (var i = 0; i < winConditions.length; i++) {
+    var tile1 = tiles[winConditions[i][0];
+    var tile2 = tiles[winConditions[i][1];
+    var tile3 = tiles[winConditions[i][2];
+    if (
+        tile1.classList.contains(player1.token) &&
+        tile2.classList.contains(player1.token) &&
+        tile3.classList.contains(player1.token)) {
+        winning();
+      } else if (tile1.classList.contains(player2.token) &&
+        tile2.classList.contains(player2.token) &&
+        tile3.classList.contains(player2.token)) {
+        tiedGame();
+      } else {
+        makeMove();
+      }
     }
   }
 
   winning() {
     this.player.wins === player.wins + 1;
-    //h2 = player wins!
-    timerCountdown();
+    results.innerText = `${this.player} wins!`
+    setTimeout();
   }
 
-  // setTimeout(function () {
-  // }, 3000};
+  tiedGame() {
+    results.innerText = `This game is a draw!`
+    setTimeout()
+  }
+
+  setTimeout(startGame() {
+  }, 3000};
 
 }
+
 
 // A Game should include:
 // A way to keep track of the data for the game board
