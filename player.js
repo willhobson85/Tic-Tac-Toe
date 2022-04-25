f05ca6b59981803d56b675dd77ecf9a060f8f04b
William Hobson <will.hobson85@gmail.com>
2022-04-25 17:55:05 -0400

    Refactor constructor, add increasewins function

--- player.js
+++ player.js
@@ -1,9 +1,12 @@
 class Player {
   constructor(name, token) {
     this.name = name;
-    this.token = undefined;
+    this.token = token;
     this.wins = 0;
     this.active = true;
   }
 
+  increaseWins() {
+    this.wins = this.wins + 1;
+  }
 }
